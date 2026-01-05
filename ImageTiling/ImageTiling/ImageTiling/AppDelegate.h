bf4c533727102d20613574082859337d629486b0
akshaypat444 <akshay.pat06@gmail.com>
2026-01-04 21:55:43 -0600

    Adding Image tiling project

---
+++ ImageTiling/ImageTiling/ImageTiling/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ImageTiling
+//
+//  Created by AKSHAY PATEL on 1/4/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ImageTiling/ImageTiling/ImageTiling/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  ImageTiling
+//
+//  Created by AKSHAY PATEL on 1/4/26.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
